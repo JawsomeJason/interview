e3badb69cab6b1a5218424ac9bd8a0e9a972c3aa
Jason T. Featheringham <thejase@gmail.com>
2016-10-26 08:23:52 -0700

    auto open

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,36 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    os = require('os'),
+    open = require('gulp-open');
+
+
+// Default usage:
+// Open one file with default application
+
+gulp.task('open', function(){
+  gulp.src('http://risa.local:4200')
+  .pipe(open());
+});
+
+
+var browser = os.platform() === 'linux' ? 'google-chrome' : (
+  os.platform() === 'darwin' ? 'google chrome' : (
+  os.platform() === 'win32' ? 'chrome' : 'firefox'));
+
+gulp.src('./package.json').pipe(open({app: 'chrome'}));
+
+
+// Simple usage, no options.
+// This will use the uri in the default browser
+
+gulp.task('uri', function(){
+  gulp.src(__filename)
+  .pipe(open({uri: 'http://risa.local:4200'}));
+});
+
+
 
 gulp.task('express', function() {
   var express = require('express');
@@ -39,6 +68,6 @@ gulp.task('watch', function() {
   gulp.watch('css/*.css', notifyLiveReload);
 });
 
-gulp.task('default', ['styles', 'express', 'livereload', 'watch'], function() {
+gulp.task('default', ['styles', 'express', 'livereload', 'watch', 'uri'], function() {
 
 });
